b57f01522db4cc9666f9a2a55920c0c66d6978bd
Winai Nadee <winain@gmail.com>
2018-11-25 07:43:57 +0000

    Delete cache.php

--- site/config/cache.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['cache_dir'] = APPPATH.'cache/cart/';
-$config['cache_default_expires'] = 0;
-/* End of file cache.php */
-/* Location: ./application/config/cache.php */
